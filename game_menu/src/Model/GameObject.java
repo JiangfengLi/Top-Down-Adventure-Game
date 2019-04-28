39dc038f9962a08400a78c4345e5a39121a3b2aa
TH <TH@DESKTOP-8K2FA7T>
2019-04-28 01:13:14 -0700

    game with menu and some buttons on it, tring to complete load game and add a background image

---
+++ game_menu/src/Model/GameObject.java
@@ -0,0 +1,86 @@
+package Model;
+
+import javafx.scene.image.Image;
+
+public abstract class GameObject {
+
+	protected Image imageFile;
+	protected int[] location = new int[2];
+	protected int[] oldLocation = new int[2];
+	protected int height;
+	protected int width;
+	protected int topHeight;
+	
+	/**
+	 * returns the object's height
+	 * @return the object's height
+	 */
+	public int getHeight() {
+		return height;
+	} 
+	
+	/**
+	 * returns the objects width
+	 * @return the objects width
+	 */
+	public int getWidth() {
+		return width;
+	}
+	
+	/**
+	 * returns the filename / location of this
+	 * object's image file
+	 * 
+	 * @return the object's image file's path.
+	 */
+	public Image getImageFile() {
+		return imageFile;
+	}
+	
+	/**
+	 * returns the location on the board of
+	 * this object
+	 * 
+	 * @return the objects location on the board
+	 */
+	public int[] getLocation() {
+		return location;
+	}
+	
+	/**
+	 * updates the object's position on the board
+	 * @param d the amount to increment/decrement x coordinate by
+	 * @param e the amount to increment/decrement y coordinate by
+	 */
+	public void updatePosition(double d, double e) {
+		oldLocation[0] = location[0];
+		oldLocation[1] = location[1];
+		location[0] += d;
+		location[1] += e;
+
+	}
+	
+	/**
+	 * sets the object's anchor point to a specific location
+	 * 
+	 * @param x the object's x coordinate
+	 * @param y the object's y coordinate
+	 */
+	public void setLocation(int x, int y) {
+		
+		oldLocation[0] = location[0];
+		oldLocation[1] = location[1];
+		
+		location[0] = x;
+		location[1] = y;
+	}
+
+	/**
+	 * returns the height of the image's top half, the half to be rendered on top
+	 * of player characters to help force perspective
+	 * @return the number of pixels of the image that need to be drawn on top of player characters
+	 */
+	public int getTopHeight() {
+		return topHeight;
+	}
+}
---
+++ game_menu/src/Model/Item.java
@@ -0,0 +1,9 @@
+package Model;
+
+/**
+ * just a shell class to help differentiate loot items from regular game objects.
+ * @author Wes Rodgers
+ *
+ */
+public abstract class Item extends GameObject{	
+} 
