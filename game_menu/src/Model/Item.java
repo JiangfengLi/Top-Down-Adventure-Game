a549082aaf6ae2b715e787bcdf6d83d9fecf2a51
Jay W Rodgers <wesrod@gmail.com>
2019-04-29 07:52:14 -0700

    Delete Item.java

--- game_menu/src/Model/Item.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-/**
- * just a shell class to help differentiate loot items from regular game objects.
- * @author Wes Rodgers
- *
- */
-public abstract class Item extends GameObject{	
-} 
